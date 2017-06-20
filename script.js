c83cc11377e913d862ed7d8a811dee057b66698c
AntonSmid <anton.smid@triera.net>
2017-06-21 00:54:22 +0200

    ball is moving and bouncing

--- script.js
+++ script.js
@@ -7,14 +7,29 @@ var x = canvas.width/2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 
-
-function draw() {
+function drawBall() {
     ctx.beginPath(); // start drawing
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095dd";
     ctx.fill();
     ctx.closePath(); // end drawing
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+
+    // odboj od stene zgoraj in spodaj
+    if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    // odboj od stene levo in desno
+    if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+
     x += dx;
     y += dy;
 }
